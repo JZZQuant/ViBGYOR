94d02c18da6df7c91ab428b530c67f71b9ea52f5
Raghava Sasikanth <v00d00childblues1@gmail.com>
2013-08-20 19:41:16 +0530

    Added button adorners to stretch and move inside dock canvas.
    ToDo:
    Time Lines
    snap to time lines
    Custom Midi Notes
    Overlapping buttons transparent click focus

---
+++ Adorners/ResizingAdorner.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ViBGYOR.Adorners
+{
+    public class ResizingAdorner : Adorner
+    {
+        // Resizing adorner uses Thumbs for visual elements.  
+        // The Thumbs have built-in mouse input handling.
+        Thumb Left, Right;
+
+        // To store and manage the adorner's visual children.
+        VisualCollection visualChildren;
+
+        // Initialize the ResizingAdorner.
+        public ResizingAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            visualChildren = new VisualCollection(this);
+
+            // Call a helper method to initialize the Thumbs
+            // with a customized cursors.
+            BuildAdornerCorner(ref Left, Cursors.SizeWE);
+            BuildAdornerCorner(ref Right, Cursors.SizeWE);
+
+            // Add handlers for resizing.
+            Left.DragDelta += new DragDeltaEventHandler(HandleLeft);
+            Right.DragDelta += new DragDeltaEventHandler(HandleRight);
+        }
+
+        // Handler for resizing from the top-right.
+        void HandleRight(object sender, DragDeltaEventArgs args)
+        {
+            FrameworkElement adornedElement = this.AdornedElement as FrameworkElement;
+            Thumb hitThumb = sender as Thumb;
+
+            if (adornedElement == null || hitThumb == null) return;
+            FrameworkElement parentElement = adornedElement.Parent as FrameworkElement;
+
+            // Ensure that the Width and Height are properly initialized after the resize.
+            EnforceSize(adornedElement);
+
+            // Change the size by the amount the user drags the mouse, as long as it's larger 
+            // than the width or height of an adorner, respectively.
+            adornedElement.Width = Math.Max(adornedElement.Width + args.HorizontalChange, hitThumb.DesiredSize.Width);
+        }
+
+        // Handler for resizing from the top-left.
+        void HandleLeft(object sender, DragDeltaEventArgs args)
+        {
+            FrameworkElement adornedElement = AdornedElement as FrameworkElement;
+            Thumb hitThumb = sender as Thumb;
+
+            if (adornedElement == null || hitThumb == null) return;
+
+            // Ensure that the Width and Height are properly initialized after the resize.
+            EnforceSize(adornedElement);
+
+            double width_old = adornedElement.Width;
+            double width_new = Math.Max(adornedElement.Width - args.HorizontalChange, hitThumb.DesiredSize.Width);
+            double left_old = Canvas.GetLeft(adornedElement);
+            adornedElement.Width = width_new;
+            Canvas.SetLeft(adornedElement, left_old - (width_new - width_old));
+        }
+
+        // Arrange the Adorners.
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            // desiredWidth and desiredHeight are the width and height of the element that's being adorned.  
+            // These will be used to place the ResizingAdorner at the corners of the adorned element.  
+            double desiredWidth = AdornedElement.DesiredSize.Width;
+            double desiredHeight = AdornedElement.DesiredSize.Height;
+            // adornerWidth & adornerHeight are used for placement as well.
+            double adornerWidth = this.DesiredSize.Width;
+            double adornerHeight = this.DesiredSize.Height;
+
+            Left.Arrange(new Rect(-adornerWidth / 2, (desiredHeight - adornerHeight) / 2, adornerWidth, adornerHeight));
+            Right.Arrange(new Rect(desiredWidth - adornerWidth / 2, (desiredHeight - adornerHeight) / 2, adornerWidth, adornerHeight));
+
+            // Return the final size.
+            return finalSize;
+        }
+
+        // Helper method to instantiate the corner Thumbs, set the Cursor property, 
+        // set some appearance properties, and add the elements to the visual tree.
+        void BuildAdornerCorner(ref Thumb cornerThumb, Cursor customizedCursor)
+        {
+            if (cornerThumb != null) return;
+
+            cornerThumb = new Thumb();
+
+            // Set some arbitrary visual characteristics.
+            cornerThumb.Cursor = customizedCursor;
+            cornerThumb.Height = cornerThumb.Width = 10;
+            cornerThumb.Opacity = 0.40;
+            cornerThumb.Background = new SolidColorBrush(Colors.MediumBlue);
+
+            visualChildren.Add(cornerThumb);
+        }
+
+        // This method ensures that the Widths and Heights are initialized.  Sizing to content produces
+        // Width and Height values of Double.NaN.  Because this Adorner explicitly resizes, the Width and Height
+        // need to be set first.  It also sets the maximum size of the adorned element.
+        void EnforceSize(FrameworkElement adornedElement)
+        {
+            if (adornedElement.Width.Equals(Double.NaN))
+                adornedElement.Width = adornedElement.DesiredSize.Width;
+            if (adornedElement.Height.Equals(Double.NaN))
+                adornedElement.Height = adornedElement.DesiredSize.Height;
+
+            FrameworkElement parent = adornedElement.Parent as FrameworkElement;
+            if (parent != null)
+            {
+                adornedElement.MaxHeight = parent.ActualHeight;
+                adornedElement.MaxWidth = parent.ActualWidth;
+            }
+        }
+        // Override the VisualChildrenCount and GetVisualChild properties to interface with 
+        // the adorner's visual collection.
+        protected override int VisualChildrenCount { get { return visualChildren.Count; } }
+        protected override Visual GetVisualChild(int index) { return visualChildren[index]; }
+    }
+}
